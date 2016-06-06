9413ed3e60d607dc9c1a2bb87ab7f09b74c4b7e0
Emmanuel Genard <edgenard@gmail.com>
2016-06-06 17:03:02 -0400

    updated clickHandlers, all tests passing

--- src/js/clickHandlers.js
+++ src/js/clickHandlers.js
@@ -1,16 +1,18 @@
+const clickCellHandler = function (e) {
+  if (this.classList.contains('dead')) {
+    this.classList.remove('dead')
+    this.classList.add('alive')
+  } else if (this.classList.contains('alive')) {
+    this.classList.remove('alive')
+    this.classList.add('dead')
+  }
+}
+
 export const addCellHandler = (selector) => {
   let cells = document.querySelectorAll(selector)
   cells = [...cells]
 
   cells.forEach((cell) => {
-    cell.addEventListener('click', (e) => {
-      if (cell.classList.contains('dead')) {
-        cell.classList.remove('dead')
-        cell.classList.add('alive')
-      } else if (cell.classList.contains('alive')) {
-        cell.classList.remove('alive')
-        cell.classList.add('dead')
-      }
-    })
+    cell.onclick = clickCellHandler
   })
 }
